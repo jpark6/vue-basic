4f88982309fcc536c2e5e57d39cc94d86a8d67eb
jpark6 <qkstjr09@gmail.com>
2022-01-19 23:48:08 +0900

    Feat: Add AppHeader Component

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <AppHeader />
+    {{ str }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
-  name: "App",
+  data: () => ({
+    str: "hi",
+  }),
   components: {
-    HelloWorld,
+    AppHeader,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
